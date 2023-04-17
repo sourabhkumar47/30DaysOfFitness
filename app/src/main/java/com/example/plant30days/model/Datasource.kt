070f781c3ddb139eb24232ebde74314526e48b4b
Sourabh Kumar <20010001030@gateway.edu.in>
2023-04-17 20:05:35 +0530

    Created data resource

--- app/src/main/java/com/example/plant30days/model/Datasource.kt
+++ app/src/main/java/com/example/plant30days/model/Datasource.kt
@@ -1,2 +1,151 @@
 package com.example.plant30days.model
 
+import com.example.plant30days.R
+
+class Data() {
+    object ExerciseRepo {
+        val exercises = listOf(
+            Exercise(
+                dayRes = R.string.day1,
+                namRes = R.string.day1_name,
+                desImage = R.drawable.biceps,
+                insRes = R.string.day1_Equipment,
+                timeRes = R.string.day1_time
+            ),
+            Exercise(
+                dayRes = R.string.day2,
+                namRes = R.string.day2_name,
+                desImage = R.drawable.biceps,
+                insRes = R.string.day2_Equipment,
+                timeRes = R.string.day2_time
+            ),
+            Exercise(
+                dayRes = R.string.day3,
+                namRes = R.string.day3_name,
+                desImage = R.drawable.biceps,
+                insRes = R.string.day3_Equipment,
+                timeRes = R.string.day3_time
+            ),
+            Exercise(
+                dayRes = R.string.day4,
+                namRes = R.string.day4_name,
+                desImage = R.drawable.biceps,
+                insRes = R.string.day4_Equipment,
+                timeRes = R.string.day4_time
+            ),
+            Exercise(
+                dayRes = R.string.day5,
+                namRes = R.string.day5_name,
+                desImage = R.drawable.biceps,
+                insRes = R.string.day5_Equipment,
+                timeRes = R.string.day5_time
+            ),
+            Exercise(
+                dayRes = R.string.day6,
+                namRes = R.string.day6_name,
+                desImage = R.drawable.biceps,
+                insRes = R.string.day6_Equipment,
+                timeRes = R.string.day6_time
+            ),
+            Exercise(
+                dayRes = R.string.day7,
+                namRes = R.string.day7_name,
+                desImage = R.drawable.biceps,
+                insRes = R.string.day7_Equipment,
+                timeRes = R.string.day7_time
+            ),
+            Exercise(
+                dayRes = R.string.day8,
+                namRes = R.string.day8_name,
+                desImage = R.drawable.biceps,
+                insRes = R.string.day8_Equipment,
+                timeRes = R.string.day8_time
+            ),
+            Exercise(
+                dayRes = R.string.day9,
+                namRes = R.string.day9_name,
+                desImage = R.drawable.biceps,
+                insRes = R.string.day9_Equipment,
+                timeRes = R.string.day9_time
+            ),
+            Exercise(
+                dayRes = R.string.day10,
+                namRes = R.string.day10_name,
+                desImage = R.drawable.biceps,
+                insRes = R.string.day10_Equipment,
+                timeRes = R.string.day10_time
+            ),
+            Exercise(
+                dayRes = R.string.day11,
+                namRes = R.string.day11_name,
+                desImage = R.drawable.biceps,
+                insRes = R.string.day11_Equipment,
+                timeRes = R.string.day11_time
+            ),
+            Exercise(
+                dayRes = R.string.day12,
+                namRes = R.string.day12_name,
+                desImage = R.drawable.biceps,
+                insRes = R.string.day12_Equipment,
+                timeRes = R.string.day12_time
+            ),
+            Exercise(
+                dayRes = R.string.day13,
+                namRes = R.string.day13_name,
+                desImage = R.drawable.biceps,
+                insRes = R.string.day13_Equipment,
+                timeRes = R.string.day13_WorkoutTime
+            ),
+            Exercise(
+                dayRes = R.string.day14,
+                namRes = R.string.day14_name,
+                desImage = R.drawable.biceps,
+                insRes = R.string.day14_Equipment,
+                timeRes = R.string.day14_WorkoutTime
+            ),
+            Exercise(
+                dayRes = R.string.day15,
+                namRes = R.string.day15_name,
+                desImage = R.drawable.biceps,
+                insRes = R.string.day15_Equipment,
+                timeRes = R.string.day15_WorkoutTime
+            ),
+            Exercise(
+                dayRes = R.string.day16,
+                namRes = R.string.day16_name,
+                desImage = R.drawable.biceps,
+                insRes = R.string.day16_Equipment,
+                timeRes = R.string.day16_WorkoutTime
+            ), Exercise(
+                dayRes = R.string.day17,
+                namRes = R.string.day17_name,
+                desImage = R.drawable.biceps,
+                insRes = R.string.day17_Equipment,
+                timeRes = R.string.day17_WorkoutTime
+            ),
+            Exercise(
+                dayRes = R.string.day18,
+                namRes = R.string.day18_name,
+                desImage = R.drawable.biceps,
+                insRes = R.string.day18_Equipment,
+                timeRes = R.string.day18_WorkoutTime
+            ),
+            Exercise(
+                dayRes = R.string.day19,
+                namRes = R.string.day19_name,
+                desImage = R.drawable.biceps,
+                insRes = R.string.day19_Equipment,
+                timeRes = R.string.day19_WorkoutTime
+            ),
+            Exercise(
+                dayRes = R.string.day20,
+                namRes = R.string.day20_name,
+                desImage = R.drawable.biceps,
+                insRes = R.string.day20_Equipment,
+                timeRes = R.string.day20_WorkoutTime
+            )
+
+
+        )
+    }
+}
