90b36be77e59f5d657e4e372c2e7e2df14a2b632
Sourabh Kumar <20010001030@gateway.edu.in>
2023-04-17 19:51:12 +0530

    created exercise data class

--- app/src/main/java/com/example/plant30days/model/Exercise.kt
+++ app/src/main/java/com/example/plant30days/model/Exercise.kt
@@ -1,3 +1,12 @@
 package com.example.plant30days.model
 
-data class Exercise()
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Exercise(
+    @StringRes val dayRes: Int,
+    @StringRes val namRes: Int,
+    @DrawableRes val desImage: Int,
+    @StringRes val insRes: Int,
+    @StringRes val timeRes: Int
+)
