6f943c68dde69d1bc08d5352b0af71c272375cc9
Sourabh Kumar <20010001030@gateway.edu.in>
2023-04-19 01:09:57 +0530

    modified layout

--- app/src/main/java/com/example/plant30days/MainActivity.kt
+++ app/src/main/java/com/example/plant30days/MainActivity.kt
@@ -10,17 +10,25 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.plant30days.model.Data.ExerciseRepo.exercises
 import com.example.plant30days.model.Exercise
 import com.example.plant30days.ui.theme.Plant30DaysTheme
@@ -44,8 +52,14 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ExerciseList() {
-
-    LazyColumn {
+    LazyColumn(
+//        modifier = Modifier
+//            .padding(
+//                start = 16.dp,
+//                end = 16.dp,
+//                top = 16.dp
+//            )
+    ) {
         items(exercises) {
             ExerciseItem(
                 exercise = it
@@ -56,8 +70,15 @@ fun ExerciseList() {
 
 @Composable
 fun ExerciseItem(exercise: Exercise, modifier: Modifier = Modifier) {
-    Card() {
-        Row() {
+    Card(
+        modifier = modifier.padding(16.dp),
+        elevation = CardDefaults.cardElevation(4.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp)
+        ) {
             ExInfo(exercise.dayRes, exercise.namRes)
             Spacer(modifier.weight(1f))
             ExIcon(exercise.desImage)
@@ -69,15 +90,17 @@ fun ExerciseItem(exercise: Exercise, modifier: Modifier = Modifier) {
 @Composable
 fun ExInfo(
     @StringRes exDay: Int,
-    @StringRes exName: Int,
-    modifier: Modifier = Modifier
+    @StringRes exName: Int
 ) {
-    Column() {
+    Column(
+    ) {
         Text(
-            text = stringResource(exDay)
+            text = stringResource(exDay),
+            Modifier.padding(top = 8.dp)
         )
         Text(
-            text = stringResource(exName)
+            text = stringResource(exName),
+            Modifier.padding(top = 8.dp)
         )
     }
 }
@@ -88,7 +111,12 @@ fun ExInfo(
 fun ExIcon(@DrawableRes exIcon: Int, modifier: Modifier = Modifier) {
     Image(
         painter = painterResource(exIcon),
-        contentDescription = null
+        contentDescription = null,
+        contentScale = ContentScale.Crop,
+        modifier = Modifier
+            .size(84.dp)
+            .padding(8.dp)
+            .clip(RoundedCornerShape(50))
     )
 }
 
